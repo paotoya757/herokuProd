f709cf8c870b7ccc587ae15210bf0bc303733c55
Pedro <pa.otoya757@uniandes.edu.co>
2015-05-04 04:28:50 -0500

    einfijifd

---
+++ src/main/java/ServerSide/Init/WebApp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServerSide.Init;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/webresources")
+public class WebApp extends ResourceConfig {
+	public WebApp(){
+        packages("ServerSide.Services");
+        packages("ServerSide.User");
+    }
+}
--- src/main/java/ServerSide/User/UserService.java
+++ src/main/java/ServerSide/User/UserService.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ServerSide.Services;
+package ServerSide.User;
 
 import ServerSide.Converters.PacienteConverter;
 import ServerSide.Init.ApiKeyEnvVariables;
